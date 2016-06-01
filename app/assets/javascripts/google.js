75cc34de17b4bc4f85bde1bead1fca9a1966f924
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-06-01 15:59:06 -0400

    adding code for searching by two or more tags

--- app/assets/javascripts/google.js
+++ app/assets/javascripts/google.js
@@ -1,70 +1,70 @@
-// $(document).ready(function(){
+// // $(document).ready(function(){
 
 	
-	function initMap() {
-	  var map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: -34.397, lng: 150.644},
-	    zoom: 6
-	  });
-	  var infoWindow = new google.maps.InfoWindow({map: map});
+// 	function initMap() {
+// 	  var map = new google.maps.Map(document.getElementById('map'), {
+// 	    center: {lat: -34.397, lng: 150.644},
+// 	    zoom: 6
+// 	  });
+// 	  var infoWindow = new google.maps.InfoWindow({map: map});
 
-	  // Try HTML5 geolocation.
-	  if (navigator.geolocation) {
-	    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+// 	  // Try HTML5 geolocation.
+// 	  if (navigator.geolocation) {
+// 	    navigator.geolocation.getCurrentPosition(function(position) {
+//       var pos = {
+//         lat: position.coords.latitude,
+//         lng: position.coords.longitude
+//       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  	}
-	}
+//       infoWindow.setPosition(pos);
+//       infoWindow.setContent('Location found.');
+//       map.setCenter(pos);
+//     }, function() {
+//       handleLocationError(true, infoWindow, map.getCenter());
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleLocationError(false, infoWindow, map.getCenter());
+//   	}
+// 	}
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
-
-// function to add markers to map for instagram posts
-// var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// var labelIndex = 0;
-
-// function initialize() {
-//   var bangalore = { lat: 12.97, lng: 77.59 };
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 12,
-//     center: bangalore
-//   });
-
-//   // This event listener calls addMarker() when the map is clicked.
-//   google.maps.event.addListener(map, 'click', function(event) {
-//     addMarker(event.latLng, map);
-//   });
-
-//   // Add a marker at the center of the map.
-//   addMarker(bangalore, map);
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
 // }
 
-// // Adds a marker to the map.
-// function addMarker(location, map) {
-//   // Add the marker at the clicked location, and add the next-available label
-//   // from the array of alphabetical characters.
-//   var marker = new google.maps.Marker({
-//     position: location,
-//     label: labels[labelIndex++ % labels.length],
-//     map: map
-//   });
-// }
+// // function to add markers to map for instagram posts
+// // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// // var labelIndex = 0;
 
-// google.maps.event.addDomListener(window, 'load', initialize);
-// });
+// // function initialize() {
+// //   var bangalore = { lat: 12.97, lng: 77.59 };
+// //   var map = new google.maps.Map(document.getElementById('map'), {
+// //     zoom: 12,
+// //     center: bangalore
+// //   });
+
+// //   // This event listener calls addMarker() when the map is clicked.
+// //   google.maps.event.addListener(map, 'click', function(event) {
+// //     addMarker(event.latLng, map);
+// //   });
+
+// //   // Add a marker at the center of the map.
+// //   addMarker(bangalore, map);
+// // }
+
+// // // Adds a marker to the map.
+// // function addMarker(location, map) {
+// //   // Add the marker at the clicked location, and add the next-available label
+// //   // from the array of alphabetical characters.
+// //   var marker = new google.maps.Marker({
+// //     position: location,
+// //     label: labels[labelIndex++ % labels.length],
+// //     map: map
+// //   });
+// // }
+
+// // google.maps.event.addDomListener(window, 'load', initialize);
+// // });
--- app/assets/javascripts/instagram.js
+++ app/assets/javascripts/instagram.js
@@ -2,87 +2,54 @@ $(document).ready(function(){
 
     var imageFound = false;
     var showImage = function(x){       
-                $('#picturecontainer').append("<img id='imageshow' src='"+x+"'></img>");
+                $('#picturecontainer').append("<img id='imageshow' src='"+x+"'></img><br>");
             }
 
-
-    function searchByLocation(keyword,lat,lng) {
-
-        // var lat= 40.6234400;
-        // var lng= -74.1575500;
-        var lat= givenLat;
-        var lng= givenLng;
-
-            // This uri allows us to search by media, within a specified location.
-            // https://www.instagram.com/developer/endpoints/media/
-
-        var uri = 'https://api.instagram.com/v1/media/search?lat='+lat+'&lng='+lng+'&access_token=3273809834.1677ed0.f58f670503ba42e28b80825f4b5dd3c6&callback=?'
-
-            // Use of jQuery here to parse through the Object
-        $.getJSON(uri, function(data) {
-                    var parsed = data
-                    console.log(parsed);
-
-            // This tells us how long the data array is
-                    x = parsed.data.length
-
-           // As we go through the different Objets
-            for(i=0; i<x; i++){
-
-                // imageFound = false;
-             // We're setting the length of the number of tags within that object as max
-                max = parsed.data[i].tags.length;
-            // As we lop through, we're cross checking the tags against our given keyword
-                    for(j=0; j<max; j++){
-            // If the keyword matches a tag from the object
-                        if(parsed.data[i].tags[j] == keyword){
-                            // alert(parsed.data[i].tags[j]);
-            // We set it as image, and then show the image.
-                var image = parsed.data[i].images.thumbnail.url;
-                            showImage(image);
-            // If keyword matches the tag, the imageFound var is set to true
-                    imageFound = true;     
-                        } else {
-
-                        }
-                }
-                       
-                }
-                    console.log(image);
-            // If the above isn't met, meaning no keyword and tag match, then we run function findBy(keyword)
-                    if(imageFound == false){
-                        findBy(keyword);
-                    }
-
-            }
-        );
-
-    }
+  
     $("#searchbutton").click(function() {
-                 $('#picturecontainer').empty();
-        searchByLocation($("#search").val(),false);
-
+        $('#picturecontainer').empty();
+        // searchByLocation($("#search").val(),false);
+        findBy($("#search").val());
      });
 
             
     function findBy(keyword) {
               // This uri allows us to search by keyword without location.
-               var uri = 'https://api.instagram.com/v1/tags/'+keyword+'/media/recent?access_token=3273809834.1677ed0.f58f670503ba42e28b80825f4b5dd3c6&callback=?&count=20'
+               var uri = 'https://api.instagram.com/v1/tags/'+keyword+'/media/recent?access_token=3291796759.1677ed0.ef402151e855498486ed25dd5f363337&callback=?&count=33&max_tag_id=AQCxFqFlbX7jvEO74uFZdZhF9Qcp6wpdMlZMpfdRSXFBJQAmciTXzh7UvVdEtwQ0z5XtbbgJFSnJD1kVK8Attw96nRHz4FwlwWQgJfxos4Mm8p_miBv9gtz658Q1R-0r9G4'
                
-            $.getJSON(uri, function(data) {
-                        var parsed = data
-                        console.log(parsed);
-                        a = parsed.data.length;
+        $.getJSON(uri, function(data) {
+                var parsed = data
+                console.log(parsed);
+                a = parsed.data.length;
 
-                        for(k=0; k<a; k++){
+                for(k=0; k<a; k++){
+                    var image = parsed.data[k].images.standard_resolution.url;
+                    console.log(image);
+                            
+                            
+                    max = parsed.data[k].tags.length;
+                // console.log(max);
+            // As we lop through, we're cross checking the tags against our given keyword
+                    for(j=0; j<max; j++){
+            // If the keyword matches a tag from the object
+                        if(parsed.data[k].tags[j].search("nyc")!=-1 == "newyorkcity"){
+                            // alert(parsed.data[i].tags[j]);
+            // We set it as image, and then show the image.
                         var image = parsed.data[k].images.standard_resolution.url;
-                            console.log(image);
-
-                            showImage(image);
+                        console.log(image);
+                        showImage(image);
+            // If keyword matches the tag, the imageFound var is set to true
+                            imageFound = true;     
                         }
+
+                    }        
+                         
+                            //showImage(image);
+                       }
                 }
             );
 
         }
 
 });
+
