51514b85257810016e8423c2b02c72d7a6e2e05e
Kirsten McAulay <mcaulay.kirsten@gmail.com>
2016-06-02 16:06:24 -0400

    squashing conflicts like henry kissenger

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160601225510) do
-
+ActiveRecord::Schema.define(version: 20160602132724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-
-  create_table "instagrams", force: :cascade do |t|
-    t.hstore   "location"
-    t.string   "tags",                    array: true
-    t.hstore   "pictures"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "grams", force: :cascade do |t|
-    t.string   "img"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "instagrams", force: :cascade do |t|
     t.string   "image"
@@ -42,20 +25,7 @@ ActiveRecord::Schema.define(version: 20160601225510) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.float    "longitude"
-    t.float    "latitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "recent_searches", force: :cascade do |t|
-    t.string   "hashtags"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
     t.string   "hashtags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
