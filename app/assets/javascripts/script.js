69b30bc0e7f9aa0f30d4e5587e909292877f0709
Kirsten McAulay <mcaulay.kirsten@gmail.com>
2016-06-03 14:38:33 -0400

    fixing code

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,59 +2,43 @@
   
     var imageFound = false;
 
-      var showImage = function(x){       
-
-
- $('#picturecontainer').append("<img id='imageshow' class='tooltip' src='assets/"+x+".jpg' width=300 height=300></img><br><br>");
-            
-    $('.tooltip').tooltipster({
-      content: $("<span><img src='https://gigaom.com/wp-content/uploads/sites/1/2013/06/new-york-2013.png'/></span>")
-                      });
-               }
+    var showImage = function(x){       
+      $('#picturecontainer').append("<img id='imageshow' class='tooltip' src='assets/"+x+".jpg' width=300 height=300></img><br><br>");
+                
+      $('.tooltip').tooltipster({
+        content: $("<span><img src='https://gigaom.com/wp-content/uploads/sites/1/2013/06/new-york-2013.png'/></span>")
+      });
+    }
 
 
        
-       var tags=["newyorkliberty","icecreamnyc","timsquare","babypizza","donuts","italy","yankees","sub","taxi","Centralpark","bridge","brooklyn","rock"]
-       $('#transbox').hide();
-       $("#searchbutton").click(function() {
-           $('#picturecontainer').empty();
+    var tags=["newyorkliberty","icecreamnyc","timsquare","babypizza","donuts","italy","yankees","sub","taxi","Centralpark","bridge","brooklyn","rock"]
+       
+    $('#transbox').hide();
+    $("#searchbutton").click(function() {
+      $('#picturecontainer').empty();
+        findBy($("#search").val());
+        $('#transbox').show();
+      });
 
-           findBy($("#search").val());
-           $('#transbox').show();
-        });
-
-       $("#search").keyup(function(e){
-          if (e.keyCode == 13) {
-            $('#picturecontainer').empty();
-            findBy($("#search").val());
-          }
-        });
+      $("#search").keyup(function(e){
+        if (e.keyCode == 13) {
+          $('#picturecontainer').empty();
+          findBy($("#search").val());
+        }
+      });
 
 
-      function findBy(keyword){
-
-       //alert(keyword);
-         
-       var index=0;
+    function findBy(keyword){ 
+      var index=0;
                    
-                   while( index<tags.length ){
-
-                         
-                      if( keyword.search(tags[index] )> -1){
-
-                        
-                           showImage(tags[index]);
-
-
-               // If keyword matches the tag, the imageFound var is set to true
-                               imageFound = true;     
-                         }
-
-                   index++;
-
-                       }        
-
-      }
-
-
+      while( index<tags.length ){
+        if( keyword.search(tags[index] )> -1){
+          showImage(tags[index]);
+             // If keyword matches the tag, the imageFound var is set to true
+          imageFound = true;     
+        }
+        index++;
+      }        
+    }
 });
